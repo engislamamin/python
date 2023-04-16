05c54a2bdc0dd22a99f09b4cdc9662773131214a
engislamamin <eng.islam.201@gmail.com>
2023-04-17 00:26:19 +0200

    Add Type conversion

---
+++ Basics/TypeConversions.py
@@ -0,0 +1,7 @@
+print(9+9.9)
+# Implicit conversion => the interpreter automatically convert one data type into another, without having to explicitly tell it to do so.
+base = 6
+heigth = 3
+area = (base*heigth)/2
+print("The area of the triangle is : " + str(area))
+# str() function to convert a number into a string .
